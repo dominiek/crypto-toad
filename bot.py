d028deec77947d2c8030088b44dd365accbfe6ce
Dominiek Ter Heide <info@dominiek.com>
2017-06-19 19:35:20 -0700

    Refactored bot a bit, added debug info

--- bot.py
+++ bot.py
@@ -6,7 +6,17 @@ import time
 import telepot
 import shelve
 
-def get_tickers():
+EXCHANGES = {
+    'poloniex': {
+        'name': 'Poloniex',
+        'url': 'https://www.poloniex.com/exchange#{}'
+    }
+}
+DEBUG_INFO = {
+    'exchanges': {}
+}
+
+def get_tickers(exchange):
     res = requests.get('https://www.poloniex.com/public?command=returnTicker')
     return json.loads(res.text).keys()
 
@@ -25,28 +35,64 @@ class Bot:
         self.bot.message_loop(self._on_message)
         self.db = shelve.open('bot.shelve.db')
 
-    def notify_ticker(self, ticker):
-        url = 'https://www.poloniex.com/exchange#{}'.format(ticker)
+    def notify_ticker(self, ticker, exchange):
+        url = exchange['url'].format(ticker)
         for user_id in self.db:
-            self.bot.sendMessage(user_id, 'Detected ticker on Poloniex! Symbol: {}, Url: {}'.format(ticker, url))
+            self.bot.sendMessage(user_id, 'Detected ticker on {}! Symbol: {}, Url: {}'.format(exchange['name'], ticker, url))
 
     def _on_message(self, msg):
         user_id = msg['from']['id']
         self.db[str(user_id)] = msg['from']
         self.db.sync()
+        if msg['text'] == '/debug':
+            text = 'Debug info:\n\n'
+            for exchange in DEBUG_INFO['exchanges']:
+                exchange_info = EXCHANGES[exchange]
+                info = DEBUG_INFO['exchanges'][exchange]
+                duration = round(time.time() - info['last_check'])
+                text += '{}: last check = {}s ago, num tickers = {}\n'.format(exchange_info['name'], duration, len(info['tickers']))
+            self.bot.sendMessage(user_id, text)
+            return
+        if msg['text'] == '/tickers':
+            text = 'I know about the following tickers:\n\n'
+            for exchange in DEBUG_INFO['exchanges']:
+                exchange_info = EXCHANGES[exchange]
+                info = DEBUG_INFO['exchanges'][exchange]
+                text += 'Poliniex:\n{}'.format(', '.join(info['tickers']))
+            self.bot.sendMessage(user_id, text)
+            return
         self.bot.sendMessage(user_id, 'Hi there. I will notify you when any new currencies hit the Poloniex exchange')
+
+class TickerTracker:
+
+    def __init__(self, bot, exchange):
+        self.bot = bot
+        self.exchange = exchange
+        self.tickers = get_tickers(exchange)
+
+    def check(self):
+        new_tickers = get_tickers(self.exchange)
+        new = diff_tickers(self.tickers, new_tickers)
+        DEBUG_INFO['exchanges'][self.exchange] = {
+            'last_check': time.time(),
+            'tickers': new_tickers
+        }
+        if len(new) > 0:
+            print('New tickers on {}!'.format(self.exchange), new)
+            for ticker in new:
+                self.bot.notify_ticker(ticker, EXCHANGES[self.exchange])
+            self.tickers = new_tickers
+            return True
+        return False
 
 def run():
     bot = Bot(os.getenv('TELEGRAM_TOKEN'))
-    old_tickers = get_tickers()
+    trackers = {}
+    for exchange in EXCHANGES:
+        trackers[exchange] = TickerTracker(bot, exchange)
     while True:
-        new_tickers = get_tickers()
-        new = diff_tickers(old_tickers, new_tickers)
-        if len(new) > 0:
-            print('New tickers!', new)
-            for ticker in new:
-                bot.notify_ticker(ticker)
-        old_tickers = new_tickers
+        for exchange in EXCHANGES:
+            trackers[exchange].check()
         time.sleep(3)
 
 run()
