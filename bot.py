fb5f98787950eb0d2aba78b6f3d1c7756e74903e
Dominiek Ter Heide <info@dominiek.com>
2017-06-19 20:03:46 -0700

    Added bittrex, liqui.io and tidex

--- bot.py
+++ bot.py
@@ -9,7 +9,22 @@ import shelve
 EXCHANGES = {
     'poloniex': {
         'name': 'Poloniex',
-        'url': 'https://www.poloniex.com/exchange#{}'
+        'url': 'https://www.poloniex.com/exchange#{}',
+        'lowercase': True
+    },
+    'bittrex': {
+        'name': 'Bittrex',
+        'url': 'https://bittrex.com/Market/Index?MarketName={}'
+    },
+    'liqui.io': {
+        'name': 'Liqui.io',
+        'url': 'https://liqui.io/#/exchange/{}',
+        'uppercase': True
+    },
+    'tidex': {
+        'name': 'Tidex',
+        'url': 'https://tidex.com/exchange/#/pair/{}',
+        'uppercase': True
     }
 }
 DEBUG_INFO = {
@@ -17,8 +32,18 @@ DEBUG_INFO = {
 }
 
 def get_tickers(exchange):
-    res = requests.get('https://www.poloniex.com/public?command=returnTicker')
-    return json.loads(res.text).keys()
+    if exchange == 'poloniex':
+        res = requests.get('https://www.poloniex.com/public?command=returnTicker')
+        return json.loads(res.text).keys()
+    if exchange == 'bittrex':
+        res = requests.get('https://bittrex.com/api/v1.1/public/getmarketsummaries')
+        return map(lambda x: x['MarketName'], json.loads(res.text)['result'])
+    if exchange == 'liqui.io':
+        res = requests.get('https://api.liqui.io/api/3/info')
+        return json.loads(res.text)['pairs'].keys()
+    if exchange == 'tidex':
+        res = requests.get('https://api.tidex.com/api/3/info')
+        return json.loads(res.text)['pairs'].keys()
 
 def diff_tickers(old_tickers, new_tickers):
     new = []
@@ -36,7 +61,12 @@ class Bot:
         self.db = shelve.open('bot.shelve.db')
 
     def notify_ticker(self, ticker, exchange):
-        url = exchange['url'].format(ticker)
+        url_ticker = ticker
+        if exchange.get('lowercase', False) == True:
+            url_ticker = url_ticker.lower()
+        if exchange.get('uppercase', False) == True:
+            url_ticker = url_ticker.upper()
+        url = exchange['url'].format(url_ticker)
         for user_id in self.db:
             self.bot.sendMessage(user_id, 'Detected ticker on {}! Symbol: {}, Url: {}'.format(exchange['name'], ticker, url))
 
@@ -44,7 +74,8 @@ class Bot:
         user_id = msg['from']['id']
         self.db[str(user_id)] = msg['from']
         self.db.sync()
-        if msg['text'] == '/debug':
+        command = msg['text'].split(' ')
+        if len(command) > 0 and command[0] == '/debug':
             text = 'Debug info:\n\n'
             for exchange in DEBUG_INFO['exchanges']:
                 exchange_info = EXCHANGES[exchange]
@@ -53,15 +84,30 @@ class Bot:
                 text += '{}: last check = {}s ago, num tickers = {}\n'.format(exchange_info['name'], duration, len(info['tickers']))
             self.bot.sendMessage(user_id, text)
             return
-        if msg['text'] == '/tickers':
-            text = 'I know about the following tickers:\n\n'
+        if len(command) > 0 and command[0] == '/tickers':
+            self.bot.sendMessage(user_id, 'I know about the following tickers:')
             for exchange in DEBUG_INFO['exchanges']:
                 exchange_info = EXCHANGES[exchange]
                 info = DEBUG_INFO['exchanges'][exchange]
-                text += 'Poliniex:\n{}'.format(', '.join(info['tickers']))
-            self.bot.sendMessage(user_id, text)
+                text = '\n{}:\n{}\n'.format(exchange_info['name'], ', '.join(info['tickers']))
+                self.bot.sendMessage(user_id, text)
             return
-        self.bot.sendMessage(user_id, 'Hi there. I will notify you when any new currencies hit the Poloniex exchange')
+        if len(command) > 2 and command[0] == '/simulate':
+            exchange = command[1]
+            if EXCHANGES.has_key(exchange):
+                exchange_info = EXCHANGES[exchange]
+                self.notify_ticker(command[2], exchange_info)
+            return
+        total_tickers = 0
+        exchange_names = []
+        for exchange in DEBUG_INFO['exchanges']:
+            exchange_info = EXCHANGES[exchange]
+            info = DEBUG_INFO['exchanges'][exchange]
+            total_tickers += len(info['tickers'])
+            exchange_names.append(exchange_info['name'])
+        text = 'Hi there, I am currently tracking {} tickers in real-time on {} and {}.\n'.format(total_tickers, ', '.join(exchange_names[0:-1]), exchange_names[-1])
+        text += '\nI will notify you instantly when I detect a new ticker!'
+        self.bot.sendMessage(user_id, text)
 
 class TickerTracker:
 
